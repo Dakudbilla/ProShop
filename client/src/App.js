cd66d67b08793e4a8f04f3fadfe2b7265501b8c3
Akudbilla Daniel <akuddev@gmail.com>
2020-10-09 23:48:04 +0000

    built product page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => (
-  <>
+  <Router>
     <Header />
     <main className="py-3">
       <Container>
-        <HomeScreen />
+        <Route exact path="/">
+          <HomeScreen />
+        </Route>
+        <Route path="/product/:id" component={ProductScreen} />
       </Container>
     </main>
     <Footer />
-  </>
+  </Router>
 );
 
 export default App;
