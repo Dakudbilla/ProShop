50a010a29c9df2d4c80afee434e00a4340257f09
Akudbilla Daniel <akuddev@gmail.com>
2020-10-12 23:02:13 +0000

    products backend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+const app = express();
+
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+connectDB();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Api Running Succesfully");
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold
+  );
+});
